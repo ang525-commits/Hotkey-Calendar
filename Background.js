ce599cfcd0acc4d7e7d5ce33e6ab960a576329da
Andrew Gambino <ang525@pitt.edu>
2026-01-30 17:37:36 -0500

    Update Background.js

--- Background.js
+++ Background.js
@@ -1,6 +1,6 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === "name-of-command-passed-into-function") {
-    chrome.tabs.update({
+    chrome.tabs.create({
       url: "https://calendar.google.com/calendar/u/0/r"
     });
   }
