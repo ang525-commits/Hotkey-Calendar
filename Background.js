bc3523c713f1e7ad5f57d9bfccc55721c9359deb
Andrew Gambino <ang525@pitt.edu>
2026-07-17 01:32:02 -0400

    Update Background.js

--- Background.js
+++ Background.js
@@ -1,7 +1,15 @@
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "name-of-command-passed-into-function") {
+  if (command === "My Calendar") {
     chrome.tabs.create({
       url: "https://calendar.google.com/calendar/u/0/r"
+    });
+  } elseif (command === "My Email") {
+    chrome.tabs.create({
+      url: "https://outlook.office.com/mail/"
+    });
+  }elseif (command === "My Drive") {
+    chrome.tabs.create({
+      url: "https://drive.google.com"
     });
   }
 });
