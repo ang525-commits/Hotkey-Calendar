40ea860311360e0c9e1b04042668bab535e7de52
Andrew Gambino <ang525@pitt.edu>
2026-08-10 00:24:33 -0400

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const save = document.getElementById('save');
-   save.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'bookmark'});
-  });
-});
